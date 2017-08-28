eb6ba1f3bb0af09f48053c03819241e83db997a6
Seyuna <p.b1994@web.de>
2017-08-29 01:55:15 +0200

    Extended S_GET_USER_LIST and added C_GET_USER_GUILD_ICON

---
+++ DamageMeter.Core/Heuristic/C_GET_USER_GUILD_LOGO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.Game.Messages;
+
+namespace DamageMeter.Heuristic
+{
+    public class C_GET_USER_GUILD_LOGO : AbstractPacketHeuristic
+    {
+        public static C_GET_USER_GUILD_LOGO Instance => _instance ?? (_instance = new C_GET_USER_GUILD_LOGO());
+        private static C_GET_USER_GUILD_LOGO _instance;
+
+        public bool Initialized = false;
+        private C_GET_USER_GUILD_LOGO() : base(OpcodeEnum.C_GET_USER_GUILD_LOGO) { }
+
+        public new void Process(ParsedMessage message)
+        {
+            base.Process(message);
+
+            if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) { return; }
+            if (message.Payload.Count < 8) { return; }
+
+            var playerId = Reader.ReadUInt32();
+            var guildId = Reader.ReadUInt32();
+            
+            if (!OpcodeFinder.Instance.KnowledgeDatabase.TryGetValue(OpcodeFinder.KnowledgeDatabaseItem.Characters, out Tuple<Type, object> currChar))
+            {
+                throw new Exception("Logger character is not in database.");
+            }
+            var chList = (Dictionary<uint, Character>)currChar.Item2;
+            if (!chList.TryGetValue(playerId, out var character)) return;
+            if (character.GuildId != guildId) return;
+
+            OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
+        }
+    }
+}
