580bebfb0fc19e0d8ae6728dbcacb94f9344eeb3
foglio <foglio1024@gmail.com>
2017-08-31 14:18:26 +0200

    added more useful methods

--- DamageMeter.Core/DbUtils.cs
+++ DamageMeter.Core/DbUtils.cs
@@ -27,11 +27,26 @@ namespace DamageMeter
             return list.Any(x => x.Cid == id && x.ZoneId == zoneId && x.TemplateId == templateId);
         }
 
+        public static bool IsUserSpawned(ulong id)
+        {
+            if (!OpcodeFinder.Instance.KnowledgeDatabase.ContainsKey(OpcodeFinder.KnowledgeDatabaseItem.SpawnedUsers)) { return false; }
+            var res = OpcodeFinder.Instance.KnowledgeDatabase[OpcodeFinder.KnowledgeDatabaseItem.SpawnedUsers].Item2;
+            var list = (List<ulong>)res;
+            return list.Any(x => x == id);
+        }
+
         public static Vector3f GetPlayerLocation()
         {
             if (!OpcodeFinder.Instance.KnowledgeDatabase.ContainsKey(OpcodeFinder.KnowledgeDatabaseItem.PlayerLocation)) { return new Vector3f(); }
             var res = OpcodeFinder.Instance.KnowledgeDatabase[OpcodeFinder.KnowledgeDatabaseItem.PlayerLocation].Item2;
             return(Vector3f) res;
+        }
+
+        public static ulong GetPlayercId()
+        {
+            if (!OpcodeFinder.Instance.KnowledgeDatabase.TryGetValue(OpcodeFinder.KnowledgeDatabaseItem.LoggedCharacter, out var res)) { return 0; }
+            var character = (LoggedCharacter) res.Item2;
+            return character.Cid;
         }
     }
 }
