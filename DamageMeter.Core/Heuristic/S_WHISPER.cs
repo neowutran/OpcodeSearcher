070c886d22e56a438809925199afeb97eb913bbb
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-08-31 23:28:24 +0300

    fix S_WHISPER

--- DamageMeter.Core/Heuristic/S_WHISPER.cs
+++ DamageMeter.Core/Heuristic/S_WHISPER.cs
@@ -91,7 +91,7 @@ namespace DamageMeter.Heuristic
                 return;
             }
 
-            if (Reader.BaseStream.Position + 2 != Reader.BaseStream.Length) //at this point, we must have reached the end of the stream
+            if (Reader.BaseStream.Position != Reader.BaseStream.Length) //at this point, we must have reached the end of the stream
             {
                 return;
             }
