1d5ac27cf7dce79216a525f885154cece2c5beeb
Neowutran <me@neowutran.ovh>
2017-08-31 17:33:01 +0200

    update loading logs

--- DamageMeter.Core/LogReader.cs
+++ DamageMeter.Core/LogReader.cs
@@ -17,6 +17,18 @@ namespace DamageMeter
         {
             var plf = new PacketLogFile(filename);
             var messageList = plf.Messages.ToList();
+            if(messageList.ElementAt(0).OpCode != 19900)
+            {
+                throw new Exception("Analysing a log without starting by the beginning is meaningless");
+            }
+            if(messageList.ElementAt(0).Payload.Count == 30) // Old log, need to remove the last 2 bytes
+            {
+                Console.WriteLine("Old log detected, removing the last 2 bytes");
+                Parallel.ForEach(messageList, message =>
+                {
+                    message.Data = new ArraySegment<byte>(message.Data.Array, 0, message.Data.Count - 2);
+                });
+            }
             return messageList;
         }
     }
