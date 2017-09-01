64e4e4cfb862aead317a3e0cefe81986b26f4792
foglio <foglio1024@gmail.com>
2017-09-01 23:36:05 +0200

    added checks to partyMemberAuthority packet

--- DamageMeter.Core/Heuristic/C_CHANGE_PARTY_MEMBER_AUTHORITY.cs
+++ DamageMeter.Core/Heuristic/C_CHANGE_PARTY_MEMBER_AUTHORITY.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 using Tera.Game.Messages;
 
 namespace DamageMeter.Heuristic
@@ -20,11 +21,13 @@ namespace DamageMeter.Heuristic
             if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) return;
             if (!OpcodeFinder.Instance.IsKnown(OpcodeEnum.S_PARTY_MEMBER_LIST)) return;
             if(message.Payload.Count != 4+4+1)return;
-            Reader.Skip(8);
+            if(!OpcodeFinder.Instance.KnowledgeDatabase.ContainsKey(OpcodeFinder.KnowledgeDatabaseItem.PartyMemberList)) return;
+            var serverId = Reader.ReadUInt32();
+            if(BasicTeraData.Instance.Servers.GetServer(serverId) == null) return;
+            var playerId = Reader.ReadUInt32();
             var canInvite = Reader.ReadByte();
             if(canInvite != 0 && canInvite != 1) return;
-
-            //TODO: add party info in DB and do more checks on that
+            if(!DbUtils.IsPartyMember(playerId, serverId)) return;
 
             OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
         }
