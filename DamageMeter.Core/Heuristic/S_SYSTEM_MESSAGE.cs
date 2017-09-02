b0c6ed66989e7f006d94afcd83df63adc2a11f16
foglio <foglio1024@gmail.com>
2017-09-02 17:23:15 +0200

    sSystemMessage

---
+++ DamageMeter.Core/Heuristic/S_SYSTEM_MESSAGE.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.Game.Messages;
+
+namespace DamageMeter.Heuristic
+{
+    class S_SYSTEM_MESSAGE : AbstractPacketHeuristic
+    {
+        public new void Process(ParsedMessage message)
+        {
+            base.Process(message);
+            if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) return;
+            if (message.Payload.Count < 2) return;
+            var offset = Reader.ReadUInt16();
+            try
+            {
+                var msg = Reader.ReadTeraString();
+                if (msg.StartsWith("@"))
+                {
+                    var i = msg.IndexOf('\v');
+                    string smt = "";
+                    smt = i != -1 ? msg.Substring(1, i - 1) : msg.Substring(1);
+                    if (!ushort.TryParse(smt, out var m)) return;
+                }
+                else return;
+            }
+            catch (Exception e) { return; }
+            OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
+        }
+    }
+}
