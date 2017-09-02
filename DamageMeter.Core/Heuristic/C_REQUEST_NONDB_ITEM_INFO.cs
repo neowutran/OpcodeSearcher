8628db631afcfbcdb98d59849dcf292d9ed4c3d2
foglio <foglio1024@gmail.com>
2017-09-02 17:42:10 +0200

    cShowItemTooltipEx, cRequestNondbItemInfo

---
+++ DamageMeter.Core/Heuristic/C_REQUEST_NONDB_ITEM_INFO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.Game.Messages;
+
+namespace DamageMeter.Heuristic
+{
+    class C_REQUEST_NONDB_ITEM_INFO : AbstractPacketHeuristic
+    {
+        public new void Process(ParsedMessage message)
+        {
+            base.Process(message);
+            if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) return;
+            if (message.Payload.Count != 3*4) return;
+            if(!OpcodeFinder.Instance.IsKnown(OpcodeEnum.S_SPAWN_ME)) return;
+            var item = Reader.ReadUInt32();
+            var unk1 = Reader.ReadUInt32();
+            var unk2 = Reader.ReadUInt32();
+            if(item == 0) return;
+            if(unk1 != 0) return;
+            if(unk2 != 0) return;
+
+
+            OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
+        }
+    }
+}
