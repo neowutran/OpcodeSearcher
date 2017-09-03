4d3bbeb3657c692a8de7a18877d9fbd042014257
foglio <foglio1024@gmail.com>
2017-09-03 13:42:11 +0200

    add sReturnToLobby, sClearAllHoldedAbormality, sPartyMemberBuffUpdate, sAccomplishAchievement

---
+++ DamageMeter.Core/Heuristic/S_RETURN_TO_LOBBY.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.Game.Messages;
+
+namespace DamageMeter.Heuristic
+{
+    class S_RETURN_TO_LOBBY : AbstractPacketHeuristic
+    {
+        private static bool waiting;
+        public static ushort PossibleOpcode;
+        public new void Process(ParsedMessage message)
+        {
+            base.Process(message);
+            if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) return;
+            if (message.Payload.Count != 0) return;
+            if (!OpcodeFinder.Instance.IsKnown(OpcodeEnum.S_PREPARE_RETURN_TO_LOBBY)) return;
+            if (!OpcodeFinder.Instance.IsKnown(OpcodeEnum.S_CLEAR_ALL_HOLDED_ABNORMALITY)) return;
+            if(!waiting) return;
+            PossibleOpcode = message.OpCode;
+        }
+
+        public static void Confirm()
+        {
+            if(!waiting) return;
+            OpcodeFinder.Instance.SetOpcode(PossibleOpcode, OpcodeEnum.S_RETURN_TO_LOBBY);
+        }
+
+        public static void Wait()
+        {
+            waiting = true;
+        }
+    }
+}
