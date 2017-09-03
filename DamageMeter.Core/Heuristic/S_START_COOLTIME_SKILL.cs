424434fedab0220ffc7fe133ffbfe80241c91a1c
foglio <foglio1024@gmail.com>
2017-09-03 03:06:52 +0200

    skill id fix

--- DamageMeter.Core/Heuristic/S_START_COOLTIME_SKILL.cs
+++ DamageMeter.Core/Heuristic/S_START_COOLTIME_SKILL.cs
@@ -12,9 +12,9 @@ namespace DamageMeter.Heuristic
             if(message.Payload.Count != 8) return;
             if(!OpcodeFinder.Instance.IsKnown(OpcodeEnum.C_START_SKILL)) return;
 
-            var skill = Reader.ReadUInt32();
-            if (C_START_SKILL.LatestSkill != skill) return;
+            var skill = Reader.ReadUInt32() - 0x04000000;
             var cd = Reader.ReadUInt32();
+            if (C_START_SKILL.LatestSkill != skill) return;
 
             OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
         }
