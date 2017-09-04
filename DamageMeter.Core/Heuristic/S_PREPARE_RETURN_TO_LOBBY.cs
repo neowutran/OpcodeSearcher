77c0a1d8ba6c3e514a10d78a15c5438a9ffef6b1
foglio <foglio1024@gmail.com>
2017-09-04 16:48:18 +0200

    check more previous packets for sReturnToLobby

--- DamageMeter.Core/Heuristic/S_PREPARE_RETURN_TO_LOBBY.cs
+++ DamageMeter.Core/Heuristic/S_PREPARE_RETURN_TO_LOBBY.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tera;
 using Tera.Game.Messages;
 
 namespace DamageMeter.Heuristic
@@ -15,18 +16,18 @@ namespace DamageMeter.Heuristic
 
             if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) { return; }
             if (message.Payload.Count != 4) { return; }
-            var previousPacket = OpcodeFinder.Instance.GetMessage(OpcodeFinder.Instance.PacketCount - 1);
-            if (previousPacket.Direction == Tera.MessageDirection.ClientToServer && previousPacket.Payload.Count == 0)
+            var time = Reader.ReadInt32();
+            if(time == 0) return;
+            for (int i = 0; i < 5; i++)
             {
-                var time = Reader.ReadInt32();
-                if (time != 0 && OpcodeFinder.Instance.KnowledgeDatabase.ContainsKey(OpcodeFinder.KnowledgeDatabaseItem.CharacterSpawnedSuccesfully))
-                {
-                    //Return To lobby detection
-                    OpcodeFinder.Instance.SetOpcode(previousPacket.OpCode, OpcodeEnum.C_RETURN_TO_LOBBY);
-                    //S_PREPARE_RETURN_TO_LOBBY
-                    OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
-                    S_CLEAR_ALL_HOLDED_ABNORMALITY.Wait();
-                }
+                var p = OpcodeFinder.Instance.GetMessage(OpcodeFinder.Instance.PacketCount - 1 - i);
+                if (p.Direction != MessageDirection.ClientToServer || p.Payload.Count != 0) { continue; }
+                //Return To lobby detection
+                OpcodeFinder.Instance.SetOpcode(p.OpCode, OpcodeEnum.C_RETURN_TO_LOBBY);
+                //S_PREPARE_RETURN_TO_LOBBY
+                OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
+                S_CLEAR_ALL_HOLDED_ABNORMALITY.Wait();
+                return;
             }
         }
     }
