7cb806993bb927354d739c9986e44d612ac67029
foglio <foglio1024@gmail.com>
2017-09-04 16:57:45 +0200

    better like this

--- DamageMeter.Core/Heuristic/S_PREPARE_RETURN_TO_LOBBY.cs
+++ DamageMeter.Core/Heuristic/S_PREPARE_RETURN_TO_LOBBY.cs
@@ -15,19 +15,22 @@ namespace DamageMeter.Heuristic
             base.Process(message);
 
             if (IsKnown || OpcodeFinder.Instance.IsKnown(message.OpCode)) { return; }
+            if(!OpcodeFinder.Instance.IsKnown(OpcodeEnum.S_SPAWN_ME)) return;
             if (message.Payload.Count != 4) { return; }
             var time = Reader.ReadInt32();
             if(time == 0) return;
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 var p = OpcodeFinder.Instance.GetMessage(OpcodeFinder.Instance.PacketCount - 1 - i);
-                if (p.Direction != MessageDirection.ClientToServer || p.Payload.Count != 0) { continue; }
-                //Return To lobby detection
-                OpcodeFinder.Instance.SetOpcode(p.OpCode, OpcodeEnum.C_RETURN_TO_LOBBY);
-                //S_PREPARE_RETURN_TO_LOBBY
-                OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
-                S_CLEAR_ALL_HOLDED_ABNORMALITY.Wait();
-                return;
+                if (p.Direction == MessageDirection.ClientToServer && p.Payload.Count == 0)
+                {
+                    //Return To lobby detection
+                    OpcodeFinder.Instance.SetOpcode(p.OpCode, OpcodeEnum.C_RETURN_TO_LOBBY);
+                    //S_PREPARE_RETURN_TO_LOBBY
+                    OpcodeFinder.Instance.SetOpcode(message.OpCode, OPCODE);
+                    S_CLEAR_ALL_HOLDED_ABNORMALITY.Wait();
+                    return;
+                }
             }
         }
     }
